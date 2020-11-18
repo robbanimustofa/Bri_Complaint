6ba7fddac042075ab9b3d1aff1f6cadb54929b82
mbastianm <m.bastianm@gmail.com>
2020-11-18 08:31:10 -0800

    adding app.css file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import Navbar from './page/customer/Navbar';
 
 function App() {
--- src/page/customer/Navbar.js
+++ src/page/customer/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return(
         <div className="navbar">
             <div className="container">
-                <a className="navbar-brand" href="#">BRI Complaint</a>
+                <a className="navbar-brand font-goldman" href="#">BRI Complaint</a>
                 <ul className="nav justify-content-end">
                     <li className="nav-item">
                         <a href="#" className="nav-link active btn-danger">Log out</a>
