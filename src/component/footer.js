1ca72c181ee88f7a7fff755f9582094ec8c6d638
robbanimustofa <robbanimustofa@gmail.com>
2020-11-19 02:06:24 +0700

    edit footer.js

--- src/component/footer.js
+++ src/component/footer.js
@@ -4,12 +4,12 @@ import Logo from "../assets/icons/briwhite.svg"
 
 function footer() {
   return (
-    <footer className="footer">
+    <footer className="footer fixed-bottom">
       <Row style={{marginLeft:"0px", marginRight:"0px"}}>
         <Col xs={5}></Col>
         <Col style={{paddingLeft:"0px", paddingRight:"0px"}} xs={5}>Copyright @2020 Group 08 for Digitalent KOMINFO</Col>
         <Col style={{paddingLeft:"0px", paddingRight:"0px", textAlign:'end'}} xs={2}>
-          <img src={Logo} alt="" />
+          <img src={Logo} alt=""  style={{marginRight:"20px"}}/>
         </Col>
       </Row>
     </footer>
