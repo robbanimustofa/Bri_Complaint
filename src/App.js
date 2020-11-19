d6f0e74dd9dee802c72148ed73447a1b583fd91b
mbastianm <m.bastianm@gmail.com>
2020-11-19 10:27:34 -0800

    adding new ticket form

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import React, { Fragment } from "react";
-import LoginCustomer from "./page/login_Customer";
-import LoginCustomerService from "./page/login_CustomerService";
-import LoginSupervisor from "./page/login_Supervisor";
-import SignupCustomer from "./page/signupCustomer";
-import DashboardSpv from "./page/spv/Dashboard_Spv"
-// import Navbar from './page/customer/Navbar';
-// import TicketListCs from './page/customer/TicketListCs';
-import FooterWeb from "./component/footer"
-// import AddCSAccount from './page/addCSAccount';
-import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css"
+import React from 'react'
+import InitTicket from './page/customer/InitTicket'
 
 function App() {
   return (
-    <BrowserRouter>
-      {/*<Navbar/>*/}
-      <div className="padding-lr-20 body">
-        {/* <AddCSAccount /> */}
-        <Fragment>
-          <Route path="/logincs" component={LoginCustomerService} exact />
-          <Route path="/" component={LoginCustomer} exact />
-          <Route path="/loginspv" component={LoginSupervisor} exact />
-          <Route path="/signupcustomer" component={SignupCustomer} exact />
-          <Route path="/dashboardspv" component={DashboardSpv} exact />
-        </Fragment>
-      </div>
-      <FooterWeb/>
-    </BrowserRouter>
+    <div>
+      <InitTicket />
+    </div>
   )
 }
 
