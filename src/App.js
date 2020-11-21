09ea15781712f0aff5795807dc7a9bffa85426a2
mbastianm <m.bastianm@gmail.com>
2020-11-21 03:04:43 -0800

    fixed background

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './App.css'
 import Footer from './component/Footer'
 import background from './background.jpg'
-// import ReplyForm from './page/spv/ReplyForm'
+import ReplyForm from './page/spv/ReplyForm'
 // import ReplyForm from './page/customer_service/ReplyForm'
-import CsList from './page/spv/CsList'
+// import CsList from './page/spv/CsList'
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", backgroundRepeat:"no-repeat", backgroundPosition:"center"}}>
-        <CsList />
-        {/* <ReplyForm /> */}
+    <div style={{ backgroundImage: `url(${background})`, backgroundSize:"cover", backgroundRepeat:"no-repeat", backgroundPosition:"center", width: "100vw", height:"100vh"}}>
+        {/* <CsList /> */}
+        <ReplyForm />
         <Footer />
     </div>
   );
