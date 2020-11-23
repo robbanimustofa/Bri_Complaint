41d8f1b727dc24d8445a2e88b448e4abf03171c7
retnoningrum <dretnon26@gmail.com>
2020-11-23 14:34:14 +0700

    post init ticket

--- src/page/customer/InitTicket.js
+++ src/page/customer/InitTicket.js
@@ -4,42 +4,62 @@ import { Form, Button } from 'react-bootstrap'
 import { Link } from "react-router-dom"
 import axios from "axios"
 
-const InitTicket = ({history}) => {
+const InitTicket = ({ history }) => {
     const linkStyle = {
         color: "black",
     };
 
     const [tittle, setTittle] = useState("");
-    const [category, setCategory] = useState("");
+    const [category, setCategory] = useState("Komplain layanan");
     const [detail, setDetail] = useState("");
-    const [screenshot, setScreenshot] = useState("");
+    const [screenshot, setScreenshot] = useState("htpp://drive.google.com/image.jpg");
     const [urlVideo, setUrlVideo] = useState("");
+    const [file, setFile] = useState([])
 
     const token = localStorage.getItem('token');
 
     const submitHandle = (e) => {
         e.preventDefault();
+        // const formData = {
+        //     file: file
+        // }
+        // axios.post("https://3671b2ca5014.ngrok.ioapi/file/upload", formData)
+        //     .then(res => {
         const ticketData = {
             complaint_name: tittle,
             categoryName: category,
+            categoryDetail: category,
             description: detail,
             screenshot: screenshot,
             url_video: urlVideo
         };
-        axios.post("https://f4d7eb9f0cc9.ngrok.io/api/customer/tickets/init-ticket", ticketData)
-          .then(res => {
-            if (res.status === 200) {
-              localStorage.setItem('token', JSON.stringify(res.data.token));
-              history.push("/dashboardcustomer")
-            } else {
-              alert(res.data.status)
-            }
-          }, [history])
-          .catch(err => {
-            console.log(err)
-            alert("Missing password");
-          })
-      }
+        // if (res.status === 200) {
+        axios.post("https://3671b2ca5014.ngrok.io/api/customer/tickets/init-ticket", ticketData,
+            {
+                headers: {
+                    "x-access-token": JSON.parse(localStorage.getItem('token'))
+                }
+            })
+            .then(res => {
+                if (res.status === 200) {
+                    history.push("/dashboardcustomer")
+                } else {
+                    alert(res.data.status)
+                }
+            }, [history])
+            .catch(err => {
+                console.log(err)
+                alert("Gagal menambahkan tiket");
+            })
+        //     } else {
+        //         alert("Gagal upload gambar")
+        //     }
+        // })
+        // .catch(err => {
+        //     console.log(err)
+        // })
+
+    }
 
     return (
         <div style={{ margin: "50px" }}>
@@ -51,8 +71,7 @@ const InitTicket = ({history}) => {
                         required
                         onChange={(e) => setTittle(e.target.value)}
                         type="text"
-                        name="tittle"
-                        placeholder="Nama Asli" />
+                        name="tittle" />
                 </Form.Group>
 
                 <Form.Group controlId="exampleForm.SelectCustom">
@@ -68,29 +87,47 @@ const InitTicket = ({history}) => {
 
                 <Form.Group controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Detail issue</Form.Label>
-                    <Form.Control as="textarea" rows={3} required />
+                    <Form.Control
+                        as="textarea"
+                        rows={3}
+                        value={detail}
+                        required
+                        onChange={(e) => setDetail(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group>
-                    <Form.File id="formcheck-api-regular">
-                        <Form.File.Label>Screenshot</Form.File.Label>
-                        <Form.File.Input />
-                    </Form.File>
+                    {/* <Form.File id="formcheck-api-regular"> */}
+                    <Form.Label>Screenshot</Form.Label>
+                    <Form.Control
+                        type="file"
+                        onChange={(e) => setFile([...file, e.target.files[0]])}
+                    />
+                    {/* <Form.File.Label>Screenshot</Form.File.Label>
+                        <Form.File.Input
+                            value={file}
+                            type="file"
+                            onChange={(e) => setFile(e.target.value[0])}
+                        /> */}
+                    {/* </Form.File> */}
                 </Form.Group>
 
                 <Form.Group>
                     <Form.Label>URL (optional)</Form.Label>
-                    <Form.Control type="text" />
+                    <Form.Control
+                        value={urlVideo}
+                        onChange={(e) => setUrlVideo(e.target.value)}
+                        type="text"
+                        name="urlVideo" />
                 </Form.Group>
 
                 <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                    <Link style={this.linkStyle} to='/dashboardcustomer'>
-                        <Button className="justify-content-end btn-danger" variant="primary" type="submit">
+                    <Link style={linkStyle} to='/dashboardcustomer'>
+                        <Button className="justify-content-end btn-danger" variant="primary">
                             Cancel
                             </Button>
                     </Link>
 
-                    <Button className="justify-content-end btn-success" variant="primary" type="submit">
+                    <Button className="justify-content-end btn-success" variant="primary" onClick={submitHandle}>
                         Submit
                         </Button>
                 </div>
